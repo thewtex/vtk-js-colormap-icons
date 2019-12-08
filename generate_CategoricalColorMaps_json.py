5c03241963a6b9a8e0972104cd8bced455d1b904
Matt McCormick <matt.mccormick@kitware.com>
2019-12-07 22:12:37 -0500

    Add CategoricalColors.json and script
    Based on colorcet:
    
      https://github.com/holoviz/colorcet

---
+++ generate_CategoricalColorMaps_json.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import csv
+import json
+from pathlib import Path
+
+glasbey_dir = Path('/home/matt/src/colorcet/assets/Glasbey')
+
+glasbey_to_name = {
+        'glasbey_bw_minc_20': 'glasbey',
+        'glasbey_bw': 'glasbey_bw',
+        'glasbey_bw_minc_20_hue_150_280': 'glasbey_cool',
+        'glasbey_bw_minc_20_hue_330_100': 'glasbey_warm',
+        'glasbey_bw_minc_20_maxl_70': 'glasbey_dark',
+        'glasbey_bw_minc_20_minl_30': 'glasbey_light',
+        }
+
+colors = []
+for file_prefix, name in glasbey_to_name.items():
+    csv_filepath = glasbey_dir / Path(file_prefix + '_n256.csv')
+    print(csv_filepath, csv_filepath.exists(), name)
+    rgb_values = []
+    with open(csv_filepath) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            rgb_values += row
+    indexed_color = {
+            'IndexedColors': rgb_values,
+            'Name': name
+            }
+    colors.append(indexed_color)
+
+with open('CategoricalColors.json', 'w') as fp:
+    json.dump(colors, fp, sort_keys=True, indent=2)
